8a35a7edbde8b565e18664376d64620f20e433fa
0kkun <shinji.okumoto.01@gmail.com>
2020-12-12 22:25:45 +0900

    add basic setting for Vue Router into app.js

--- server/resources/js/app.js
+++ server/resources/js/app.js
@@ -7,27 +7,29 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue');
+//Vue.jsの読み込み
+window.Vue = require('vue'); 
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
+// Vue Routerの読み込み
+import VueRouter from 'vue-router'; 
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+// Vue.jsで、Vue Routerを使うように設定
+Vue.use(VueRouter); 
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-const app = new Vue({
-    el: '#app'
+// vue-routerによるルーティング設定
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        { path: '/', component: require('./components/list.vue'), name: 'list' }, // ルートでアクセスしたら、List.vueを表示
+        { path: '/create', component: require('./components/Form.vue'), name: 'create' }, // createにアクセスしたらForm.vueを表示
+        { path: '/:id', component: require('./components/Detail.vue'), name: 'detail' }, // id番号でアクセスしたらDetail.vueを表示
+    ]
 });
+
+// Vueのコンポーネント。ページ上部にメニューバーを表示させたいので、Navbar.vueを登録
+Vue.component('navbar', require('./components/Navbar.vue')); 
+
+// Vue.jsの実行
+const app = new Vue({
+    router
+}).$mount('#app');
