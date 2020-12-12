2327485e31f5805d4d6f2bfd579f7a23a676eef0
0kkun <shinji.okumoto.01@gmail.com>
2020-12-12 17:03:53 +0900

    edit resouce for topic

--- server/app/Http/Resources/Topic.php
+++ server/app/Http/Resources/Topic.php
@@ -11,12 +11,19 @@ class Topic extends JsonResource
 {
     /**
      * Transform the resource into an array.
+     * Eloquentを元にして、どの項目がAPIとして返されるかを設定
      *
      * @param  \Illuminate\Http\Request  $request
      * @return array
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        // return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'content' => $this->content,
+            'date' => $this->created_at->format('Y-m-d H:i:s'),
+        ];
     }
 }
